bc44e5ccb6e7b1e9f2744f6412e8870108c799fd
k <k@KwonSeungUk>
2017-09-05 17:01:21 +0900

    TEST2

--- blog/src/main/java/com/yedam/blog/view/users/UsersController.java
+++ blog/src/main/java/com/yedam/blog/view/users/UsersController.java
@@ -7,27 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yedam.blog.biz.users.UsersService;
 
-@Controller
-public class UsersController {
 
-	@Autowired
-	UsersService usersService;
-	
-	@RequestMapping("/getUsersList.do")
-	public String getUsers(){
-		System.out.println(usersService.getUsersList());
-		return "/users/login";
-	}
-	
-	@RequestMapping("/getBlogAdmin.do")
-	public String getBlogAdmin() {
-		return "/blogAdmin/admin";
-	}
-	
-	@RequestMapping("/test.do")
-	public String getTest() {
-		return "/blogAdmin/test";
-=======
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
